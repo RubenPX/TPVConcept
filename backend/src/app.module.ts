811cb12805126b0c884278b6b0827f63425bd21a
RubenPX <rubenmarchante@gmail.com>
2024-02-29 14:31:32 +0100

    ✨ Generate productos y facturas

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { TrabajadoresModule } from './trabajadores/trabajadores.module';
+import { ProductosModule } from './productos/productos.module';
+import { FacturasModule } from './facturas/facturas.module';
 
 @Module({
-  imports: [AuthModule, TrabajadoresModule],
+  imports: [AuthModule, TrabajadoresModule, ProductosModule, FacturasModule],
 })
 export class AppModule {}
